9371643d13559305ef694807bd2a5d20bd17ff10
Shawn Johnson <shawn.johnson@bhegts.com>
2024-02-27 08:53:33 -0500

    refactor keyboard

--- frontend/src/components/Keypad.js
+++ frontend/src/components/Keypad.js
@@ -13,6 +13,24 @@ export default function Keypad({ usedKeys, onLetterClick }) {
     []
   );
 
+  // const rowKeys1 = lettersVal[0];
+  // const rowKeys2 = lettersVal[1];
+  // const rowKeys3 = lettersVal[2];
+
+  // const rowKeys1 = lettersVal[0].map((key) => ({ key }));
+  const rowKeys1 = useMemo(
+    () => lettersVal[0].map((key) => ({ key })),
+    [lettersVal]
+  );
+  const rowKeys2 = useMemo(
+    () => lettersVal[1].map((key) => ({ key })),
+    [lettersVal]
+  );
+  const rowKeys3 = useMemo(
+    () => lettersVal[2].map((key) => ({ key })),
+    [lettersVal]
+  );
+
   useEffect(() => {
     setLetters(lettersVal.flat().map((key) => ({ key })));
   }, [setLetters, lettersVal]);
@@ -51,22 +69,51 @@ export default function Keypad({ usedKeys, onLetterClick }) {
 
   return (
     <div className="keypad">
-      {letters &&
-        letters.map((l, index) => {
-          const color = usedKeys[l.key];
-          const isActive = activeKey === l.key;
-          return (
-            <div
-              key={index}
-              className={`${color} ${isActive ? "active" : ""} key`}
-              onClick={() => handleLetterClick(l.key)}
-              onMouseOver={() => handleMouseOver(l.key)}
-              onMouseOut={handleMouseOut}
-            >
-              {l.key}
-            </div>
-          );
-        })}
+      {rowKeys1.map((l, index) => {
+        const color = usedKeys[l.key];
+        const isActive = activeKey === l.key;
+        return (
+          <div
+            key={index}
+            className={`${color} ${isActive ? "active" : ""} key`}
+            onClick={() => handleLetterClick(l.key)}
+            onMouseOver={() => handleMouseOver(l.key)}
+            onMouseOut={handleMouseOut}
+          >
+            {l.key}
+          </div>
+        );
+      })}
+      {rowKeys2.map((l, index) => {
+        const color = usedKeys[l.key];
+        const isActive = activeKey === l.key;
+        return (
+          <div
+            key={index}
+            className={`${color} ${isActive ? "active" : ""} key`}
+            onClick={() => handleLetterClick(l.key)}
+            onMouseOver={() => handleMouseOver(l.key)}
+            onMouseOut={handleMouseOut}
+          >
+            {l.key}
+          </div>
+        );
+      })}
+      {rowKeys3.map((l, index) => {
+        const color = usedKeys[l.key];
+        const isActive = activeKey === l.key;
+        return (
+          <div
+            key={index}
+            className={`${color} ${isActive ? "active" : ""} key`}
+            onClick={() => handleLetterClick(l.key)}
+            onMouseOver={() => handleMouseOver(l.key)}
+            onMouseOut={handleMouseOut}
+          >
+            {l.key}
+          </div>
+        );
+      })}
     </div>
   );
 }
